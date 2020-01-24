c7b026c594a2c2b7a2c057ea0bfe993c7544f6e4
Kunihiko MIYOSHI <miyoshik@gmail.com>
2020-01-24 23:30:41 +0900

    extract product list for shizuoka pcd

--- jp-pcd-metadata-fetcher/jppcdmetadatafetcher/fetcher.py
+++ jp-pcd-metadata-fetcher/jppcdmetadatafetcher/fetcher.py
@@ -5,18 +5,19 @@ import os
 import requests
 
 
-class downloader(luigi.Task):
+class TextDownloader(luigi.Task):
     filepath = luigi.Parameter()
     url = luigi.Parameter()
+    decode = luigi.Parameter(default='utf-8')
 
     def output(self):
         return luigi.LocalTarget(
-            format=luigi.format.Nop, path=self.filepath)
+            format=luigi.format.UTF8, path=self.filepath)
 
     def run(self):
         r = requests.get(self.url)
-        with self.output().open('wb') as f:
-            f.write(r.content)
+        with self.output().open('w') as f:
+            f.write(r.content.decode(self.decode))
 
 
 class ShizuokaPCDProductList(luigi.Task):
@@ -24,13 +25,29 @@ class ShizuokaPCDProductList(luigi.Task):
         'https://pointcloud.pref.shizuoka.jp/lasmap/ankenmapsrc?request=MarkerSet&Xmax=139.6856689453125&Xmin=137.08053588867188&Ymax=36.097938036628065&Ymin=33.83962341851979')
 
     def requires(self):
-        return downloader(os.path.join('tmp', 'shizuoka-pcd-product-list.txt'), self.url)
+        return TextDownloader(os.path.join('tmp', 'shizuoka-pcd-product-list.txt'), self.url)
 
     def output(self):
         return luigi.LocalTarget(os.path.join('shizuokapcd', 'products.json'))
 
     def run(self):
-        pass
+        with self.input().open() as f:
+            rawdata = f.read()
+        records = []
+        for raw in rawdata.split('?'):
+            row = raw.split(':')
+            if len(row) < 4:
+                continue
+            record = {
+                'id': row[0],
+                'name': row[1],
+                'lon': float(row[2]),
+                'lat': float(row[3]),
+            }
+            records.append(record)
+
+        with self.output().open('w') as f:
+            json.dump(records, f, indent=2, ensure_ascii=False)
 
 
 def command():
